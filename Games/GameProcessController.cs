f3bc51949bcf23473e73c5a7229ca1edbf8907c0
Maximilien Noal <noal.maximilien@gmail.com>
2021-06-29 20:41:29 +0200

    Revert "Fix incompatibilites with DOSBox-X"
    This reverts commit abb591b37b2b4fdb6ddbd2b96f839cd09277d7ec.

--- Games/GameProcessController.cs
+++ Games/GameProcessController.cs
@@ -157,6 +157,10 @@ namespace AmpShell.Games
             {
                 UseShellExecute = true
             };
+            if (StringExt.IsNullOrWhiteSpace(this.gameInstance.DBConfPath) == false)
+            {
+                psi.WorkingDirectory = Path.GetDirectoryName(this.gameInstance.DBConfPath);
+            }
 
             if (StringExt.IsNullOrWhiteSpace(args) == false)
             {
